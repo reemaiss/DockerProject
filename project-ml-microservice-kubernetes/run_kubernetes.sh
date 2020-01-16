a78be6ac0e2547b0a44e13ada6c81f0fd4a0fd90
Rema Ibrahim Alsuwailm <ralsuwailm@elm.sa>
2020-01-16 21:04:57 +0300

    First commit

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=2030rema/api
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run remaalsuwailm --image=2030rema/api --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward remaalsuwailm-7748cf774f-mkzs5 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,11 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=2030rema/api
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push 2030rema/api:latest
